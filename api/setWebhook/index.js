b95d7faa4aa9862baf2c133263c2c587b786ffa2
Christian Ceciliano <cristiancehi@gmail.com>
2019-04-29 11:39:49 -0500

    fix: token on simple functions

--- api/setWebhook/index.js
+++ api/setWebhook/index.js
@@ -1,9 +1,11 @@
 const { SECRET } = process.env
 const { body, url } = require('../tgUtils')
 const _fetch = require('node-fetch')
+const _url = require('url')
 
 module.exports = (req, res) => {
-  if (req.query.token !== SECRET) {
+  const query = _url.parse(req.url).query
+  if (query.token !== SECRET) {
     return res.status(403).send({ error: 'No authorized' })
   }
   _fetch(
