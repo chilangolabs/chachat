348a56d1a7b1a6e6eab79b124fa814be0d816962
Christian Ceciliano <cristiancehi@gmail.com>
2019-04-29 19:46:02 -0500

    fix token issues

--- api/deleteWebhook/index.js
+++ api/deleteWebhook/index.js
@@ -4,9 +4,10 @@ const _fetch = require('node-fetch')
 const _url = require('url')
 
 module.exports = (req, res) => {
-  const query = _url.parse(req.url).query
+  const query = _url.parse(req.url, true).query
   if (query.token !== SECRET) {
-    return res.status(403).send({ error: 'No authorized' })
+    res.writeHead(403, { 'Content-Type': 'application/json' })
+    return res.end(JSON.stringify({ error: 'No authorized' }))
   }
   _fetch(url('deleteWebhook'))
     .then(res => res.text())
